a1dddcec64cd74a2aa87079672b02cb3514a5dfa
Sebastian Moreno Acero <sysma1997@gmail.com>
2024-07-26 16:43:52 -0500

    Best performance to render multiple cubes

--- SysmaEngine/Main.cpp
+++ SysmaEngine/Main.cpp
@@ -35,18 +35,18 @@ int main() {
 		return static_cast<float>(rand() % limit);
 	};
 
-	E2D::Rectangle* rectangle{ new E2D::Rectangle{ shader } };
+	E2D::Rectangle rectangle{ shader };
 
 	while (engine->isLoop()) {
 		engine->newFrame();
 
 		if (Engine::KeyJustPressed(GLFW_KEY_ESCAPE)) engine->closeLoop();
 		if (Engine::KeyPressed(GLFW_KEY_SPACE)) {
-			rectangle->size = glm::vec2{ 50.0f };
-			rectangle->position = glm::vec2{ addRandValue(Engine::Width), addRandValue(Engine::Height)};
-			rectangle->color = addColor();
+			rectangle.size = glm::vec2{ 50.0f };
+			rectangle.position = glm::vec2{ addRandValue(Engine::Width), addRandValue(Engine::Height)};
+			rectangle.color = addColor();
 
-			vectors.push_back(rectangle);
+			vectors.push_back(new E2D::Rectangle{rectangle});
 		}
 		if (Engine::KeyJustPressed(GLFW_KEY_P)) {
 			std::cout << "Num cubes: " << vectors.size() << '\n';
